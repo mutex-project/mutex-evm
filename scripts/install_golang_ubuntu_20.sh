75e06f457fa3822b07215cad5fac3bc5f3d9e305
rotavele <rotavele@protonmail.ch>
2020-10-11 15:30:01 -0700

    ...

--- scripts/install_golang_ubuntu_20.sh
+++ scripts/install_golang_ubuntu_20.sh
@@ -3,3 +3,7 @@ sudo apt-get update;
 sudo apt-get install golang build-essential -y;
 git clone https://github.com/mutex-project/mutex-evm.git;
 cd ./mutex-evm && make && mv ./build/bin/geth ./build/bin/mutex;
+
+
+
+
