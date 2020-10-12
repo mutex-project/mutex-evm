68fbff1181daa0feb456f5c19c2e11bf0055459d
rotavele <rotavele@protonmail.ch>
2020-10-12 11:03:38 -0700

    CI@1602459775

--- scripts/install_genesis.sh
+++ scripts/install_genesis.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-git pull;
 rm -rf ~/.mutex/geth/chaindata;
 rm -rf ~/.mutex/geth/lightchaindata;
 rm -rf ~/.mutex/geth/ethhash;
 ./scripts/init_node.sh;
-./scripts/start_node.sh;
--- scripts/start_mining.sh
+++ scripts/start_mining.sh
@@ -6,5 +6,7 @@
   --rpccorsdomain="*" \
   --nousb \
   --rpcvhosts=* \
+  --mine \
+  --minerthreads 2 \
   --etherbase '0xaef86364cbe0554c983a592745a92d66a3249404' 
  
